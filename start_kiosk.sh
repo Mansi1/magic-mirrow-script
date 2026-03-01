e72d4569b56f4783fa430814126769a02d8540e6
Michael Mannseicher <michael.mannseicher@doctarigroup.com>
2026-03-01 22:27:21 +0100

    refactor: some updates

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-# Disable screen blanking
+
+# 1. Prevent the screen from turning off or going to sleep
 xset s noblank
 xset s off
 xset -dpms
 
-# Start the Window Manager
+# 2. Start a tiny window manager (prevents Chromium from "floating" awkwardly)
 matchbox-window-manager -use_titlebar no &
 
-# Start Chromium in Kiosk Mode
-chromium --noerrdialogs --disable-infobars --kiosk http://localhost:8080
+# 3. Launch Chromium in Kiosk Mode
+# Extra flags added to save RAM and CPU on the Pi Zero 2 W
+chromium \
+  --noerrdialogs \
+  --disable-infobars \
+  --kiosk \
+  --disable-gpu \
+  --check-for-update-interval=31536000 \
+  --num-raster-threads=2 \
+  http://localhost:8080
